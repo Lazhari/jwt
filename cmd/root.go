b2727658c08bc936b84c25557f8b0f3e48c891e7
Mohammed Lazhari <contact@molazhari.dev>
2026-01-22 01:20:28 +0100

    Remove unused boilerplate from root command

--- cmd/root.go
+++ cmd/root.go
@@ -1,3 +1,5 @@
+// Package cmd provides the command-line interface implementation for the JWT CLI tool.
+// It includes commands for signing, decoding, and inspecting JWT tokens.
 package cmd
 
 import (
@@ -11,9 +13,6 @@ var rootCmd = &cobra.Command{
 	Use:   "jwt",
 	Short: "JWT CLI tool for signing and decoding tokens",
 	Long:  `A CLI tool to sign and decode JWT tokens using various algorithms.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -23,16 +22,4 @@ func Execute() {
 	if err != nil {
 		os.Exit(1)
 	}
-}
-
-func init() {
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.jwt-cli.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
